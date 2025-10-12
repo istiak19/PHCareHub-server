1b2cdc5ba9e870afa0613e41b86af9f240ea6974
istiak19 <istiak15-14128@diu.edu.bd>
2025-10-12 10:59:31 +0600

    role updated

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,10 +4,11 @@ import { validateRequest } from "../../middlewares/validateRequest";
 import { UserValidation } from "./user.validation";
 import { fileUploader } from "../../helpers/fileUploader";
 import { checkAuth } from "../../middlewares/checkAuth";
+import { role } from "../../../constants/roles";
 
 const router = Router();
 
-router.get("/", checkAuth("PATIENT"), userController.getAllUser);
+router.get("/", checkAuth(role.admin), userController.getAllUser);
 router.post("/create-admin", fileUploader.upload.single("file"), validateRequest(UserValidation.createAdminValidation), userController.createAdmin);
 router.post("/create-doctor", fileUploader.upload.single("file"), validateRequest(UserValidation.createDoctorValidation), userController.createDoctor);
 
---
+++ src/constants/roles.ts
@@ -0,0 +1,5 @@
+export const role = {
+    patient: "PATIENT",
+    doctor: "DOCTOR",
+    admin: "ADMIN"
+} as const;
