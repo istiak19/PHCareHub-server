1c0baab0f3474d6d8daa60305270f9e74fd901d2
istiak19 <istiak15-14128@diu.edu.bd>
2025-10-13 16:02:48 +0600

    updated

--- src/app/modules/schedule/schedule.route.ts
+++ src/app/modules/schedule/schedule.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { scheduleController } from "./schedule.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { role } from "../../../constants/roles";
 
 const router = Router();
 
-router.get("/", scheduleController.scheduleForDoctor);
+router.get("/", checkAuth(role.doctor, role.admin), scheduleController.scheduleForDoctor);
 router.post("/create-schedule", scheduleController.createSchedule);
 router.delete("/:id", scheduleController.deleteSchedule);
 
