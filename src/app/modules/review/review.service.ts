c262cf3299f84603e6800da1e246ebb3e985f9bb
istiak19 <istiak15-14128@diu.edu.bd>
2025-10-21 20:11:41 +0600

    review created

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -1,7 +1,63 @@
+import httpStatus from 'http-status';
 import { JwtPayload } from "jsonwebtoken";
+import { prisma } from "../../shared/prisma";
+import { AppError } from "../../errors/AppError";
+import { IReview } from './review.interface';
 
-const createReview = async (token: JwtPayload, payload: any) => {
+const createReview = async (token: JwtPayload, payload: IReview) => {
+    const isExistPatient = await prisma.patient.findUnique({
+        where: {
+            email: token.email
+        }
+    });
 
+    if (!isExistPatient) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Patient not found");
+    };
+
+    const isExistAppointment = await prisma.appointment.findUnique({
+        where: {
+            id: payload.appointmentId
+        }
+    });
+
+    if (isExistAppointment?.patientId !== isExistPatient.id) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Appointment not found");
+    };
+
+    const reviewData = await prisma.$transaction(async (tnx) => {
+        const result = await tnx.review.create({
+            data: {
+                appointmentId: payload.appointmentId,
+                comment: payload.comment,
+                rating: payload.rating,
+                doctorId: isExistAppointment.doctorId,
+                patientId: isExistPatient.id
+            }
+        });
+
+        const avgRating = await tnx.review.aggregate({
+            _avg: {
+                rating: true
+            },
+            where: {
+                doctorId: isExistAppointment.doctorId
+            }
+        });
+
+        await tnx.doctor.update({
+            where: {
+                id: isExistAppointment.doctorId
+            },
+            data: {
+                averageRating: avgRating._avg.rating as number
+            }
+        });
+
+        return result;
+    });
+
+    return reviewData;
 };
 
 export const reviewService = {
