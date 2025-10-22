c122847c0f1cd7dec45f0e3abb8b3c2b6c56d6dd
istiak19 <istiak15-14128@diu.edu.bd>
2025-10-22 20:02:09 +0600

    updated profile doctor

--- src/app/modules/doctor/doctor.route.ts
+++ src/app/modules/doctor/doctor.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { role } from "../../../constants/roles";
 import { doctorController } from "./doctor.controller";
+import { fileUploader } from "../../helpers/fileUploader";
 
 const router = Router();
 
@@ -9,6 +10,7 @@ router.post("/suggestion", doctorController.getAISuggestions);
 router.get("/", checkAuth(role.admin, role.doctor, role.patient), doctorController.getAllDoctor);
 router.get("/:id", checkAuth(role.admin, role.doctor, role.patient), doctorController.getByDoctor);
 router.patch("/:id", doctorController.updateDoctor);
+router.patch("/profile/:id", checkAuth(role.doctor), fileUploader.upload.single("file"), doctorController.updateDoctorProfile);
 router.delete("/:id", doctorController.deleteDoctor);
 
 export const doctorRouter = router;
